99fdc86f29e5077b9bb70f8ea9b1069c2db81c3b
Apostolos Eleftheriou <95987435+Apostolos-Eleftheriou@users.noreply.github.com>
2022-03-02 14:12:09 +0200

    added carousel

---
+++ script.js
@@ -0,0 +1,7 @@
+$(document).ready(function(){
+    $('.carousel').slick({
+        slidesToShow: 4, slidesToScroll: 4,
+        prevArrow:  '<i class="fa-solid fa-chevron-left"></i>',
+        nextArrow: '<i class="fa-solid fa-chevron-right"></i>'
+    });
+  });
