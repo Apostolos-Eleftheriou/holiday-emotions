c0f193afcf69248e705f2cb8e1d1c2ac27fe773a
Apostolos Eleftheriou <95987435+Apostolos-Eleftheriou@users.noreply.github.com>
2022-03-02 19:00:09 +0200

    added fade in

--- script.js
+++ script.js
@@ -5,3 +5,20 @@ $(document).ready(function(){
         nextArrow: '<i class="fa-solid fa-chevron-right"></i>'
     });
   });
+
+  $(document).on("scroll", function() {
+    var pageTop = $(document).scrollTop();
+    var pageBottom = pageTop + $(window).height();
+    var tags = $(".tag");
+  
+    for (var i = 0; i < tags.length; i++) {
+      var tag = tags[i];
+  
+      if ($(tag).position().top < pageBottom) {
+        $(tag).addClass("visible");
+      } else {
+        $(tag).removeClass("visible");
+      }
+    }
+  });
+  
