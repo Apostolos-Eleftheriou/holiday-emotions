623255abec692a9bf8294a4e1f0b56eca5fe7219
Apostolos Eleftheriou <95987435+Apostolos-Eleftheriou@users.noreply.github.com>
2022-03-11 13:04:13 +0200

    prog

--- script.js
+++ script.js
@@ -80,6 +80,42 @@ $(document).ready(function(){
     });
   });
 
+  $(document).ready(function(){
+    $('.carousel-mobile-three').slick({
+        slidesToShow: 1, slidesToScroll: 1,
+        prevArrow:  '<i class="fa-solid fa-angle-left"></i>',
+        nextArrow: '<i class="fa-solid fa-angle-right"></i>',
+        appendArrows: $('.arrows-mobile-three'),
+        responsive: [
+          {
+            breakpoint: 992,
+            settings: {
+              centerMode: true,
+              slidesToShow: 3
+            }
+          },{
+            breakpoint: 930,
+            settings: {
+              centerMode: true,
+              slidesToShow: 2
+            }
+          },
+          {
+            breakpoint: 670,
+            settings: {
+              slidesToShow: 2
+            }
+          },
+          {
+            breakpoint: 600,
+            settings: {
+              centerMode: true,
+              slidesToShow: 1
+            }
+          }
+        ]
+    });
+  });
   
   $(document).on("scroll", function() {
     var pageTop = $(document).scrollTop();
